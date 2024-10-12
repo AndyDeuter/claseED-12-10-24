22956a917e057b076c76da2bf0244f0f0cd323aa
Andy <Andydeuter2021@gmail.com>
2024-10-12 13:46:45 -0600

    arregalando errores

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,11 +1,12 @@
 class Rutina:
     def __init__(self):
         self.actividades = {}
-def agregar_actividad(self, actividad, tiempo):
-    self.actividades[actividad] = tiempo
-def mostrar_rutina(self):
-    for actividad, tiempo in self.actividades.items():
-        print(f"{actividad}: {tiempo} horas")
+    def agregar_actividad(self, actividad, tiempo):
+        self.actividades[actividad] = tiempo
+    def mostrar_rutina(self):
+        for actividad, tiempo in self.actividades.items():
+            print(f"{actividad}: {tiempo} horas")
+
 rutina = Rutina()
 rutina.agregar_actividad('Despertarse', 0.5)
 rutina.agregar_actividad('Ducharse', 0.5)
