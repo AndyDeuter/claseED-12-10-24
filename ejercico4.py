dab7520cab8132d01620214bc6e1617db571023d
Andy <Andydeuter2021@gmail.com>
2024-10-12 13:42:57 -0600

    agregar ejercicio 4

---
+++ ejercico4.py
@@ -0,0 +1,33 @@
+class GrafoMusical:
+    def __init__(self):
+        self.grafo = {}
+    def agregar_cancion(self, cancion):
+        if cancion not in self.grafo:
+            self.grafo[cancion] = []
+    def agregar_transicion(self, cancion_origen, cancion_destino):
+        if cancion_origen in self.grafo and cancion_destino in self.grafo:
+            self.grafo[cancion_origen].append(cancion_destino)
+    def mostrar_grafo(self):
+        for cancion, transiciones in self.grafo.items():
+            print(f"{cancion} puede saltar a: {', '.join(transiciones) if transiciones
+        else 'Ninguna'}")
+            def buscar_cancion(self, cancion):
+                if cancion in self.grafo:
+                    transiciones = self.grafo[cancion]
+                    return f"{cancion} puede saltar a: {', '.join(transiciones) if transiciones
+        else 'Ninguna'}"
+            return f"{cancion} no se encuentra en el grafo."
+reproductor = GrafoMusical()
+
+reproductor.agregar_cancion('Canción A')
+reproductor.agregar_cancion('Canción B')
+reproductor.agregar_cancion('Canción C')
+reproductor.agregar_cancion('Canción D')
+reproductor.agregar_transicion('Canción A', 'Canción B')
+reproductor.agregar_transicion('Canción A', 'Canción C')
+reproductor.agregar_transicion('Canción B', 'Canción D')
+reproductor.agregar_transicion('Canción C', 'Canción D')
+reproductor.mostrar_grafo()
+cancion_buscar = input("")
+resultado = reproductor.buscar_cancion(cancion_buscar)
+print(resultado)
